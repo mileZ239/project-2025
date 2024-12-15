52cf7faa3f9ed34b32550a805188d850a81f5e11
mileZ239 <imperia2008m@yandex.ru>
2024-12-15 21:36:52 +0300

    Added delay to bats

--- bat.py
+++ bat.py
@@ -1,5 +1,4 @@
 import random
-
 import pygame
 
 
@@ -20,16 +19,22 @@ class Bat:
             self.flyingRange = random.randint(flyingRange // 2, flyingRange * 3 // 2)
         else:
             self.flyingRange = flyingRange
-        self.delay = 1000
+        self.paused = 0
         self.sprite = pygame.image.load('assets/bat.png')
         self.rect = self.sprite.get_rect(center=(self.x, self.y))
         self.name = 'bat'
 
     def run(self):
+        if self.paused > 0:
+            self.paused -= 1
+            self.display.blit(self.sprite, self.rect)
+            return
         self.x += self.speedX
         self.y += self.speedY
         if abs(self.x - self.baseX) + abs(self.y - self.baseY) >= self.flyingRange:
             self.speedX = -self.speedX
             self.speedY = -self.speedY
+            # How many frames to stay still?
+            self.paused = 30
         self.rect = self.sprite.get_rect(center=(self.x, self.y))
         self.display.blit(self.sprite, self.rect)
